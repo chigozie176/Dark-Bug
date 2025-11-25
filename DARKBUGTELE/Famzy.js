64e93ca2b61b5dd5d9dc332fbebf1ebaae50d58e
chigozie176 <gozieezeh2005@gmail.com>
2025-11-25 16:49:19 +0000

    Add files via upload

---
+++ DARKBUGTELE/Famzy.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "8577376196:AAF6igA16HhRkYtnBWZXmkglSLde9IVMB58", // Token bot Telegram
+  OWNER_ID: "7528793664", // ID owner
+  allowedGroupIds: [-1002401306074], // ID group 
+};
